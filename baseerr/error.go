66d15348d9433a3b28571bd5df245a5c092d085b
Cory Redmond <code@cory.red>
2021-05-23 03:42:43 +0100

    Unmarshal error

--- baseerr/error.go
+++ baseerr/error.go
@@ -2,6 +2,7 @@ package baseerr
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 )
 
@@ -44,4 +45,19 @@ func (a AppError) MarshalJSON() ([]byte, error) {
 		Err:  a.Err.Error(),
 		Data: a.Data,
 	})
+}
+
+func (a *AppError) UnmarshalJSON(data []byte) error {
+	var tempS = struct {
+		Err  string                 `json:"error"`
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	if err := json.Unmarshal(data, &tempS); err != nil {
+		return err
+	}
+
+	a.Data = tempS.Data
+	a.Err = errors.New(tempS.Err)
+	return nil
 }
